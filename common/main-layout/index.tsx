fc67d0f638419b119da6c9e91f51dadea677c243
saber2pr <1029985799@qq.com>
2020-11-02 13:52:40 +0800

    update

--- common/main-layout/index.tsx
+++ common/main-layout/index.tsx
@@ -7,6 +7,7 @@ import { MainFooter } from '../main-footer'
 import Head from 'next/head'
 
 export interface MainLayout {
+  rootClassName?: string
   className?: string
   children: ReactNode
   title?: string
@@ -14,11 +15,12 @@ export interface MainLayout {
 
 export const MainLayout = ({
   className,
+  rootClassName,
   children,
   title = '标题',
 }: MainLayout) => {
   return (
-    <div className={classnames('MainLayout', className)}>
+    <div className={classnames('MainLayout', rootClassName)}>
       <Head>
         <title>{title}</title>
       </Head>
@@ -26,7 +28,7 @@ export const MainLayout = ({
         <MainHeader />
       </div>
       <main className="MainLayout-Main">
-        <div className="MainContent">{children}</div>
+        <div className={classnames('MainContent', className)}>{children}</div>
       </main>
       <div className="MainLayout-Footer">
         <MainFooter />

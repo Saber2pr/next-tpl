310b7f46d7b468a0d916acb6e49d0f3bb1da678d
saber2pr <1029985799@qq.com>
2020-12-01 17:09:33 +0800

    feat

---
+++ scripts/reset-branch.sh
@@ -0,0 +1,12 @@
+# master
+git checkout master
+# reset testing
+git branch -D testing
+git push origin :testing
+git checkout -b testing
+git push origin testing
+# reset dev
+git branch -D dev
+git push origin :dev
+git checkout -b dev
+git push origin dev

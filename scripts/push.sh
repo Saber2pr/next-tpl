4bbd3d2aae2dafb86a3ce9f380aa897e9e90bff2
saber2pr <1029985799@qq.com>
2020-12-15 10:02:14 +0800

    update

--- scripts/push.sh
+++ scripts/push.sh
@@ -1,9 +1,11 @@
 # push dev
 git checkout dev
-git push
+git push --set-upstream origin dev
 # push testing
 git checkout testing
-git push
+git merge dev
+git push --set-upstream origin testing
 # push master
 git checkout master
+git merge testing
 git push

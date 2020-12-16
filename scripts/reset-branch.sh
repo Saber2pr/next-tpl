cb800318443a364412c81740a4c1b218c0966c87
saber2pr <1029985799@qq.com>
2020-12-16 15:01:05 +0800

    update

--- scripts/reset-branch.sh
+++ scripts/reset-branch.sh
@@ -4,9 +4,9 @@ git checkout master
 git branch -D testing
 git push origin :testing
 git checkout -b testing
-git push origin testing
+git push --set-upstream origin testing
 # reset dev
 git branch -D dev
 git push origin :dev
 git checkout -b dev
-git push origin dev
+git push --set-upstream origin dev

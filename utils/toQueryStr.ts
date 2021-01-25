61c7f3ed67d96f24c511e356a27543a7acbf8522
saber2pr <1029985799@qq.com>
2021-01-25 16:16:46 +0800

    update

--- utils/toQueryStr.ts
+++ utils/toQueryStr.ts
@@ -1,1 +1,12 @@
-export const toQueryStr = (obj: any) => new URLSearchParams(obj).toString()
+export const toQueryStr = (obj: any) => {
+  if (obj) {
+    for (const key in obj) {
+      if (obj[key] === null || obj[key] === undefined) {
+        delete obj[key]
+      }
+    }
+    return new URLSearchParams(obj).toString()
+  } else {
+    return ''
+  }
+}
